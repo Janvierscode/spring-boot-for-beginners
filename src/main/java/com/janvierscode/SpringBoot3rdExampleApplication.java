1287f4e5d9490814bc54dee5724965abf70e1f0a
janvier <iyakaremyejanvier1@gnmail.com>
2024-03-04 11:54:13 +0200

    initializing spring boot sproject

---
+++ src/main/java/com/janvierscode/SpringBoot3rdExampleApplication.java
@@ -0,0 +1,13 @@
+package com.janvierscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3rdExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3rdExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/janvierscode/SpringBoot3rdExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janvierscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3rdExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
